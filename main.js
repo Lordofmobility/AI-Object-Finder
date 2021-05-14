fcb23ef6af645188e01732a657dc64bc13a64a70
Ishan Rangnekar <72988033+WhiteStudent@users.noreply.github.com>
2021-05-14 15:13:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-function start() {
+video = ""
+object = ""
+status = ""
 
+function setup() {
+    canvas = createCanvas(480, 345)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.hide()
+}
+
+
+function draw() {
+    image(video, 0, 0, 480, 345)
+}
+
+function start() {
+    object = document.getElementById("object_identification")   
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded)
+    document.getElementById("status").innerHTML = "Status: Detecting Objects"
+}
+
+function modelLoaded() {
+    console.log("Object Detection Successfully Started");
+    status = true
 }
